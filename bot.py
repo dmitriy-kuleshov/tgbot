8728bcc9a82866b619ce5a69d8b1dfb68a01d76d
dimitrius <dkuleshov0506@gmail.com>
2024-08-06 14:23:00 +0300

    создание обработчика сообщений пользователя

--- bot.py
+++ bot.py
@@ -1,5 +1,17 @@
 import telebot
 from telebot import types
+
 bot = telebot.TeleBot('7187442707:AAE269XiCH13-F72cjWZL-QkZwE5j7IZnps')
 
 
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем могу помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Поздоровайся чёрт")
+    else:
+        bot.send_message(message.from_user.id, "Ты что глупый кок?, напиши /help.")
+
+
+bot.polling(none_stop=True, interval=0)
