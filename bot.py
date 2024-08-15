2c7d309928af9d02a3594efc5fc6734019473dda
dimitrius <dkuleshov0506@gmail.com>
2024-08-15 23:06:03 +0300

    зарегистрировал обработчики вне класса убрал глобальные переменные

--- bot.py
+++ bot.py
@@ -1,22 +1,12 @@
 import telebot
 from telebot import types
 import psycopg2
+
 # from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 # import schedule
 # import requests
 
 bot = telebot.TeleBot('7187442707:AAE269XiCH13-F72cjWZL-QkZwE5j7IZnps')
-
-# @bot.message_handler(content_types=['text'])
-# def get_text_messages(message):
-#     if message.text == "Привет":
-#         bot.send_message(message.from_user.id, "Привет, чем могу помочь?")
-#     elif message.text == "/help":
-#         bot.send_message(message.from_user.id, "Поздоровайся чёрт")
-#     else:
-#         bot.send_message(message.from_user.id, "Ты что глупый кок?, напиши /help.")
-
-bot.polling(none_stop=True, interval=0)
 
 
 # schedule.every().day.at('15.46').do(start)
@@ -31,7 +21,7 @@ class RegistrationUser:
 
     @bot.message_handler(content_types=['text'])
     def start(self, message):
-
+        print("Получено сообщение:", message.text)
         if message.text == '/reg':
             bot.send_message(message.from_user.id, "Как тебя зовут?")
             bot.register_next_step_handler(message, self.get_name)  # следующий шаг – функция get_name
@@ -39,7 +29,7 @@ class RegistrationUser:
             bot.send_message(message.from_user.id, 'Напиши /reg')
 
     def get_name(self, message):  # получаем имя
-        global name
+        self.name = message.text
         name = message.text
         keyboard_gender = types.InlineKeyboardMarkup()
         keyboard_gender.add(types.InlineKeyboardButton('Муж.', callback_data='option1'))
@@ -48,29 +38,27 @@ class RegistrationUser:
 
     @bot.callback_query_handler(func=lambda call: call.data in ['option1', 'option2'])
     def callback_for_options(self, call):
-        global gender
 
         if call.data == 'option1':
-            gender = 'Мужской'
-            bot.send_message(call.message.chat.id, 'Мужчинский мужчина - уважаю')
+            self.gender = 'Мужской'
+            # bot.send_message(call.message.chat.id, 'Мужчинский мужчина - уважаю')
         elif call.data == 'option2':
-            gender = 'Женский'
-            bot.send_message(call.message.chat.id, 'ААААААА женщина')
+            self.gender = 'Женский'
+            # bot.send_message(call.message.chat.id, 'ААААААА женщина')
 
         # После выбора пола спрашиваем фамилию
         bot.send_message(call.message.chat.id, 'Какая у тебя фамилия?')
         bot.register_next_step_handler(call.message, self.get_surname)
 
     def get_surname(self, message):
-        global surname
+        self.surname = message.text
         surname = message.text
-        bot.send_message(message.from_user.id, 'Сколько тебе лет?')
+        bot.send_message(message.from_user.id, 'Сколько тебе лует?')
         bot.register_next_step_handler(message, self.get_age)
 
     def get_age(self, message):
-        global age
         try:
-            age = int(message.text)  # Проверяем, что возраст введен корректно
+            self.age = int(message.text)  # Проверяем, что возраст введен корректно
         except ValueError:
             bot.send_message(message.from_user.id, 'Цифрами, пожалуйста')
             bot.register_next_step_handler(message, self.get_age)
@@ -82,7 +70,7 @@ class RegistrationUser:
         key_no = types.InlineKeyboardButton(text='Нет', callback_data='no')
         keyboard.add(key_no)
 
-        question = f'Тебе {age} лет, тебя зовут {name} {surname}, пол: {gender}. Всё верно?'
+        question = f'Тебе {self.age} лет, тебя зовут {self.name} {self.surname}, пол: {self.gender}. Всё верно?'
         bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
 
     @bot.callback_query_handler(func=lambda call: call.data in ['yes', 'no'])
@@ -90,8 +78,31 @@ class RegistrationUser:
         global age
         if call.data == "yes":
             bot.send_message(call.message.chat.id,
-                             f'Имя твоё запомню я: {name} {surname} - любитель наяривать по вечерам')
+                             f'Имя твоё запомню я: {self.name} {self.surname} - любитель наяривать по вечерам')
         elif call.data == "no":
             age = 0  # сброс возраста
         bot.send_message(call.message.chat.id, 'Давай попробуем еще раз. Сколько тебе лет?')
         bot.register_next_step_handler(call.message, self.get_age)
+
+
+user_registration = RegistrationUser()
+
+
+# Регистрируем обработчики
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    user_registration.start(message)
+
+
+@bot.callback_query_handler(func=lambda call: call.data in ['option1', 'option2'])
+def handle_gender_callback(call):
+    user_registration.callback_for_options(call)
+
+
+@bot.callback_query_handler(func=lambda call: call.data in ['yes', 'no'])
+def handle_confirmation_callback(call):
+    user_registration.callback_worker(call)
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
