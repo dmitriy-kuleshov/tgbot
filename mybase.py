34493402b53f9ca119c512ea5521b26c5bd5ff4b
dimitrius <dkuleshov0506@gmail.com>
2024-08-19 23:01:43 +0300

    скрипты создания таблицы, ввода данных в неё

--- mybase.py
+++ mybase.py
@@ -8,10 +8,28 @@ try:
         password=password,
         database=db_name
     )
+    # для создания таблицы в БД нужен коммит
+    connection.autocommit = True
     with connection.cursor() as cursor:
         cursor.execute(
             "SELECT version();"
         )
+# создание таблицы в БД
+    # with connection.cursor() as cursor:
+    #     cursor.execute(
+    #         """CREATE TABLE botskiy(
+    #         id serial PRIMARY KEY,
+    #         name varchar(50) NOT NULL)"""
+    #     )
+    #     print("[INFO] Table created successfully (ZZZ)")
+# вставка данных в БД
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """INSERT INTO botskiy (name) VALUES
+            ('zorobotskiy');"""
+        )
+        print("[INFO] Data was inserted successfully (ZZZ)")
+# извлечение данных из БД
 
         print(f"Server version: {cursor.fetchone()}")
 except Exception as _ex:
