ed7eb9b281a1945ccc9aee42e7865a6c99bf6ebb
dimitrius <dkuleshov0506@gmail.com>
2024-08-21 22:42:52 +0300

    объединил логику в один обработчик

--- handlers/bot.py
+++ handlers/bot.py
@@ -1,14 +1,26 @@
 import telebot
 from config import TOKEN
-#from handlers import registration, product_existence
-# import schedule
-# import requests
-from registration import register_handlers
-from product_existence import search_handlers
+from registration import RegistrationUser
+from product_existence import ProductExistence
+
 bot = telebot.TeleBot(TOKEN)
 
-register_handlers(bot)
-search_handlers(bot)
+user_registration = RegistrationUser()
+product_existence = ProductExistence()
+
+
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    if message.text == '/reg':
+        user_registration.start_registration(bot, message)
+    elif message.text == '/product':
+        product_existence.start_search(bot, message)
+    else:
+        bot.send_message(message.from_user.id, 'Неизвестная команда. Используйте /reg или /product.')
+
+
+# Обработчики для callback_query можно добавить здесь, если они требуются
 
 if __name__ == '__main__':
+    print("Bot is polling...")
     bot.polling(none_stop=True)
