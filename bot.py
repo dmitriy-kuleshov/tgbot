c6038ec11668e3962682079dfeb939291e06f661
dimitrius <dkuleshov0506@gmail.com>
2024-08-21 21:44:52 +0300

    добавил вызов функции из модуля поиска товара

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import telebot
 from config import TOKEN
-from handlers import registration
+from handlers import registration, product_existence
 
 # import schedule
 # import requests
@@ -8,6 +8,7 @@ from handlers import registration
 bot = telebot.TeleBot(TOKEN)
 
 registration.register_handlers(bot)
+product_existence.search_handlers(bot)
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
